818c7f420c8dd2557b07cb336e8d27a44b752ddd
yangzhenzhuozz <yangzhenzhuozz@126.com>
2023-08-04 00:41:26 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <RouterLink to="/">hello world</RouterLink>
+  </header>
+  <RouterView />
+</template>
